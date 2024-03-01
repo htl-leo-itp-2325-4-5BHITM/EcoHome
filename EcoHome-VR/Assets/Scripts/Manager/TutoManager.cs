92da91b96642af17e4a655b3925fc2c12d2d5ade
kaiserErkol <kaiserErkol@users.noreply.github.com>
2024-03-01 15:47:07 +0100

    worked on TutoManager

--- EcoHome-VR/Assets/Scripts/Manager/TutoManager.cs
+++ EcoHome-VR/Assets/Scripts/Manager/TutoManager.cs
@@ -39,6 +39,13 @@ public class TutoManager : MonoBehaviour
     // Input Data from right/left Controller
     private InputData _inputData;
 
+    bool playTutorial = true;
+    bool endTutorial = false;
+
+    bool usedRightController = false;
+    bool usedLeftController = false;
+    bool usedHead = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,29 +56,35 @@ public class TutoManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 leftVelocity))
-        {
-            Debug.Log("getting velocity data from Controller");
-            Debug.Log("Velocity Value: " + leftVelocity);
-        }*/
-        
+        // If-Anweisung muss zu einem "OnLoad" Abfrage umgeschrieben werden
+        if (playTutorial) {
+            UpdateGameState(TutorialState.StartOfGame);
+            this.playTutorial = false;
+        }
+
         if (_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out Vector2 leftThumbStick))
         {
-            Debug.Log("getting thumbStick movement: " + leftThumbStick);
+            Debug.Log("getting left_thumbStick movement: " + leftThumbStick);
+            //check the distance of the movement and set the variable to true
+            this.usedLeftController = true;
+        }
+
+        if (_inputData._rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out Vector2 rightThumbStick))
+        {
+            Debug.Log("getting right_thumbStick movement: " + rightThumbStick);
+            //check the distance of the movement and set the variable to true
+            this.usedRightController = true;
         }
     }
 
     public void UpdateGameState(TutorialState newState)
     {
-        State = newState;
-
         switch (newState) {
             case TutorialState.StartOfGame:
                 playStart();
                 break;
             case TutorialState.LearnMovement:
-                this.isLearnMovement = false;
+                playLearnMovement(this.usedLeftController, this.usedRightController, this.usedHead);
                 break;
             case TutorialState.LearnPickUp:
                 break;
@@ -87,15 +100,49 @@ public class TutoManager : MonoBehaviour
     }
 
     private void playStart() {
-        audioPlayer.PlayOneShot(_clip1);
+        StartCoroutine(playClipWithDelay(_clip1, 7));
         UpdateGameState(TutorialState.LearnMovement);
     }
 
-    private void playLearnMovement(bool isLearnMovement) {
-        audioPlayer.PlayOneShot(_clip3);
-        UpdateGameState(TutorialState.LearnMovement);
+    private void playLearnMovement(bool isLeftUsed, bool isRightUsed, bool isHeadUsed) {
+        if (isLeftUsed && isRightUsed && isHeadUsed)
+        {
+            UpdateGameState(TutorialState.LearnPickUp);
+        }
+        else {
+            StartCoroutine(playClipWithDelay(_clip2, 2));
+
+            if (isHeadUsed)
+            {
+                if (isLeftUsed)
+                {
+                    StartCoroutine(playClipWithDelay(_clip3, 2));
+
+                    if (isRightUsed)
+                    {
+                        StartCoroutine(playClipWithDelay(_clip4, 2));
+                        UpdateGameState(TutorialState.LearnPickUp);
+                    }
+                }
+            }
+        }     
+    }
+
+    private void playLearnPickUp() {
+
+    }
+
+    IEnumerator playClipWithDelay(AudioClip clip, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (!endTutorial || clip == _clip8)
+        {
+            audioPlayer.Stop();
+            audioPlayer.PlayOneShot(clip);
+        }
     }
 }
+
 public enum TutorialState {
     StartOfGame,
     LearnMovement,
