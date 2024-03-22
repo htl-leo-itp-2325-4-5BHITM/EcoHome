f6ee794d4e7b4794651fa8fd2ddfa0f675464882
sfeichtenschlager <feichtenschlager.s@gmx.at>
2024-03-22 09:07:35 +0100

    Loading Screen Prototype

--- EcoHome-VR/Assets/Scripts/MainMenuInteractor.cs
+++ EcoHome-VR/Assets/Scripts/MainMenuInteractor.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuInteractor : MonoBehaviour
 {
     private AsyncOperation _asyncOperation;
+    public GameObject LoadingScreen;
+    public Slider LoadingBarFill;
+
+    private void Start() 
+    {
+        LoadingScreen = GameObject.Find("LoadingScreen");
+        LoadingBarFill = GameObject.Find("Slider").GetComponent<Slider>();
+    }
 
     private IEnumerator LoadSceneAsyncProcess(string sceneName)
     {
         this._asyncOperation = SceneManager.LoadSceneAsync(sceneName);
         this._asyncOperation.allowSceneActivation = false;
 
+        LoadingScreen.SetActive(true);
+
         while (!this._asyncOperation.isDone)
         {
+            float progress = Mathf.Clamp01(_asyncOperation.progress / 0.9f);
+            LoadingBarFill.value = progress;
+
             yield return null;
         }
     }
