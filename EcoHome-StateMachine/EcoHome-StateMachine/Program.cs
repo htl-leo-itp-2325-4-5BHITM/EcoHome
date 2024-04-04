a931916d5a3525be7a43cb702557b6e1f78325c4
sfeichtenschlager <feichtenschlager.s@gmx.at>
2024-04-04 09:42:33 +0200

    Initial commit State Machine and UML

---
+++ EcoHome-StateMachine/EcoHome-StateMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EcoHome_StateMachine
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
