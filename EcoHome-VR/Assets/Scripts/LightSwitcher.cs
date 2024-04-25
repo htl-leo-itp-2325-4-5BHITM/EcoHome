01eb9e4c024f88264b2b21749e87552954f60f3a
sfeichtenschlager <feichtenschlager.s@gmx.at>
2024-04-25 09:20:57 +0200

    Added lights

---
+++ EcoHome-VR/Assets/Scripts/LightSwitcher.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitcher : MonoBehaviour
+{
+
+    private string pressedPair;
+    private GameObject pairedLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        pressedPair = this.gameObject.name.Split('_')[1];
+        pairedLight = GameObject.Find("ceilingLight_" + pressedPair + "/Point Light");
+
+        if(pairedLight.activeSelf == true) 
+        {
+            pairedLight.SetActive(false);
+        }
+    }
+}
