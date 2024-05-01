1ee000ebe0616d50a30dddd3f7d4ce2e7472ddad
sfeichtenschlager <--null>
2024-05-01 17:57:45 +0200

    Added desk light functionality

--- EcoHome-VR/Assets/Scripts/LightSwitcher.cs
+++ EcoHome-VR/Assets/Scripts/LightSwitcher.cs
@@ -6,6 +6,7 @@ public class LightSwitcher : MonoBehaviour
 {
     private string pressedPair;
     private GameObject[] ceilingLights;
+    private GameObject[] deskLights;
 
 
     // Start is called before the first frame update
@@ -13,7 +14,11 @@ public class LightSwitcher : MonoBehaviour
     {
         ceilingLights = new GameObject[GameObject.FindGameObjectsWithTag("cLightSource").Length];
         System.Array.Copy(GameObject.FindGameObjectsWithTag("cLightSource"), 0, ceilingLights, 0, GameObject.FindGameObjectsWithTag("cLightSource").Length);
-        System.Array.Sort(ceilingLights, (a,b) => { return a.name.CompareTo( b.name); });
+        System.Array.Sort(ceilingLights, (a,b) => { return a.name.CompareTo(b.name); });
+
+        deskLights = new GameObject[GameObject.FindGameObjectsWithTag("dLightSource").Length];
+        System.Array.Copy(GameObject.FindGameObjectsWithTag("dLightSource"), 0, deskLights, 0, GameObject.FindGameObjectsWithTag("dLightSource").Length);
+        System.Array.Sort(deskLights, (a,b) => { return a.name.CompareTo(b.name); });
     }
 
     // Update is called once per frame
@@ -30,6 +35,27 @@ public class LightSwitcher : MonoBehaviour
         foreach (GameObject obj in ceilingLights)
         {
             if (obj.name == "cLightSource_" + pressedPair)
+            {
+                if(obj.activeSelf == true) 
+                {
+                    obj.SetActive(false);
+                }
+                else
+                {
+                    obj.SetActive(true);
+                }
+                break;
+            }
+        }
+    }
+
+    public void PressDeskLight(GameObject pressedSwitch)
+    {
+        pressedPair = pressedSwitch.name.Split('_')[2];
+
+        foreach (GameObject obj in deskLights)
+        {
+            if (obj.name == "dLightSource_" + pressedPair)
             {
                 if(obj.activeSelf == true) 
                 {
