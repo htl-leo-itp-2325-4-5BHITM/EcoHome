5e408a7310a4cdd8153bbba23dcf42e38a54a65f
sfeichtenschlager <--null>
2024-05-01 17:25:11 +0200

    Finalized ceiling light functionality

--- EcoHome-VR/Assets/Scripts/LightSwitcher.cs
+++ EcoHome-VR/Assets/Scripts/LightSwitcher.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class LightSwitcher : MonoBehaviour
 {
-
     private string pressedPair;
-    private GameObject pairedLight;
+    private GameObject[] ceilingLights;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ceilingLights = new GameObject[GameObject.FindGameObjectsWithTag("cLightSource").Length];
+        System.Array.Copy(GameObject.FindGameObjectsWithTag("cLightSource"), 0, ceilingLights, 0, GameObject.FindGameObjectsWithTag("cLightSource").Length);
+        System.Array.Sort(ceilingLights, (a,b) => { return a.name.CompareTo( b.name); });
     }
 
     // Update is called once per frame
@@ -20,14 +22,26 @@ public class LightSwitcher : MonoBehaviour
         
     }
 
-    public void PressLightswitch()
+    public void PressLightswitch(GameObject pressedSwitch)
     {
-        pressedPair = this.gameObject.name.Split('_')[1];
-        pairedLight = GameObject.Find("ceilingLight_" + pressedPair + "/Point Light");
+        pressedSwitch.transform.Find("Lichtschalter").transform.Rotate(0,0,180);
+        pressedPair = pressedSwitch.name.Split('_')[1];
 
-        if(pairedLight.activeSelf == true) 
+        foreach (GameObject obj in ceilingLights)
         {
-            pairedLight.SetActive(false);
+            if (obj.name == "cLightSource_" + pressedPair)
+            {
+                if(obj.activeSelf == true) 
+                {
+                    obj.SetActive(false);
+                }
+                else
+                {
+                    obj.SetActive(true);
+                }
+                break;
+            }
         }
+
     }
 }
