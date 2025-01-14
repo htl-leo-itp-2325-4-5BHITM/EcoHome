247dd2f2b13b247139e86c5e2604ba1a6a0574bc
Yasin Erkol <91544005+kaiserErkol@users.noreply.github.com>
2025-01-14 18:53:28 +0100

    test aim stuff

--- EcoHome-VR/Assets/Scripts/Tutorial/AimingDetection.cs
+++ EcoHome-VR/Assets/Scripts/Tutorial/AimingDetection.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-
+using TMPro;
 public class AimingDetection : MonoBehaviour
 {
     private XRRayInteractor rayInteractor;
     private XRInteractionManager interactionManager;
+    private GameObject lastAimedObject;
 
     void Start()
     {
@@ -19,18 +20,32 @@ public class AimingDetection : MonoBehaviour
             GameObject aimedObject = hit.collider.gameObject;
             Debug.Log("Currently aiming at: " + aimedObject.name);
 
-            // Add custom logic for interactions here
-            HighlightObject(aimedObject);
+            if (lastAimedObject != aimedObject)
+            {
+                ResetTextMeshPro(lastAimedObject);
+
+                // Add custom logic for interactions here
+                HighlightObject(aimedObject);
+            }
         }
         else
         {
             // No object is aimed at
+            ResetTextMeshPro(lastAimedObject);
+            lastAimedObject = null;
             Debug.Log("Not aiming at any object.");
         }
     }
 
     void HighlightObject(GameObject obj)
     {
+        TextMeshPro textMeshPro = obj.GetComponent<TextMeshPro>();
+
+        if (textMeshPro != null)
+        {
+            textMeshPro.enabled = false;
+        }
+
         // Example: Change object's color when aimed at
         Renderer renderer = obj.GetComponent<Renderer>();
         if (renderer != null)
@@ -38,4 +53,22 @@ public class AimingDetection : MonoBehaviour
             renderer.material.color = Color.yellow;
         }
     }
+
+    void ResetTextMeshPro(GameObject obj)
+    {
+        if (obj == null) return;
+
+        TextMeshPro textMeshPro = obj.GetComponent<TextMeshPro>();
+        if (textMeshPro != null)
+        {
+            textMeshPro.enabled = true;
+        }
+
+        Renderer renderer = obj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = Color.white;
+        }
+    }
+
 }
